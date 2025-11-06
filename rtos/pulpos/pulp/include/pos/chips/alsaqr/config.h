d726531c6decc9c0fab4e754c6f47f6b823d6a6c
Daniele Benassi <benassi.d@outlook.it>
2025-11-06 10:11:41 +0100

    Extendend rtos/pulpos/pulp/include

---
+++ rtos/pulpos/pulp/include/pos/chips/alsaqr/config.h
@@ -0,0 +1,17 @@
+//EDIT
+
+#ifndef __POS__CHIPS__ALSAQR__CONFIG_H__
+#define __POS__CHIPS__ALSAQR__CONFIG_H__
+
+#include "archi/pulp_defs.h"
+
+#define PULP_CHIP CHIP_PULP
+#define PULP_CHIP_FAMILY CHIP_PULP
+#define CONFIG_PULP 1
+#define PULP_CHIP_STR alsaqr
+#define PULP_CHIP_FAMILY_STR alsaqr
+#define ARCHI_CORE_HAS_PULPV2 1
+
+#define ARCHI_CORE_HAS_1_10 1
+
+#endif
